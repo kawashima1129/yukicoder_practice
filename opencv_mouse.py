3f617b6ed4fa358c2a5a5e5829a85741ef4c5994
kawashima1129 <k.ktak1129@gmail.com>
2017-09-29 14:16:52 +0900

    [modify]続けてファイルを読み込めるように変更 　　　　システムの不安定性を改善

--- opencv_mouse.py
+++ opencv_mouse.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 26 15:59:32 2017
-
-@author: leon
+@author: Katsuaki Kawashima
 """
 
 import cv2
@@ -10,19 +9,11 @@ import numpy as np
 import wx
 import sys
 
-SAVE_FLAG = False
-LBUTTONDOWN_FLAG = False
-LBUTTONUP_FLAG = False
-MOUSEMOVE_FLAG = False
-xy1 = [0, 0]
-xy2 = [0, 0]
-
-
 def mouse_event(event, x, y, flags, param):
     global LBUTTONDOWN_FLAG, LBUTTONUP_FLAG, MOUSEMOVE_FLAG, xy1, xy2
     global CompositeImg
-    CompositeImg = img.copy()
-    
+    CompositeImg = img.copy() 
+
     if event == cv2.EVENT_LBUTTONDOWN:
         LBUTTONDOWN_FLAG = True
         xy1 = x, y 
@@ -33,11 +24,12 @@ def mouse_event(event, x, y, flags, param):
             xy2 = x, y
             cv2.rectangle(CompositeImg, (xy1[0], xy1[1]), (xy2[0], xy2[1]), (0, 255, 0), 2)
             cv2.imshow('title', CompositeImg)
-
-               
+            
+            
     if event == cv2.EVENT_LBUTTONUP:
         if LBUTTONDOWN_FLAG == True and MOUSEMOVE_FLAG == True:
-            LBUTTONUP_FLAG = True #クリックした後，離すとフラグが立つ
+            LBUTTONUP_FLAG = True
+            MOUSEMOVE_FLAG = False
             saveTF()
         elif MOUSEMOVE_FLAG == False:
             LBUTTONDOWN_FLAG = False    
@@ -52,11 +44,10 @@ def saveTF():
         global SAVE_FLAG
         SAVE_FLAG = True
     elif result == wx.ID_NO:
-        cv2.imshow('title',img)
         LBUTTONDOWN_FLAG = False
         LBUTTONUP_FLAG = False
         MOUSEMOVE_FLAG = False
-       
+    app = None  
 
 def Adjust(xy1, xy2):
     _xy1 = np.array(xy1)
@@ -102,31 +93,52 @@ def Trimming(img, _xy1, _xy2):
 
          
 if __name__ == '__main__':
-    app = wx.App()
-    dialog = wx.FileDialog(None, u'ファイルを選択してください')
-    dialog.ShowModal()
-    file_path = dialog.GetPath()
-    img = cv2.imread(file_path)
-    
-    if img is None:
-         dialog = wx.MessageBox(u'画像が読み込めません', u'ユーザーメッセージ')
-         app = None
-         sys.exit(0)
-         
-    cv2.namedWindow('title')
     while(True):
-        cv2.setMouseCallback('title', mouse_event)
-        cv2.imshow('title', CompositeImg)
-        if cv2.waitKey(1) == ord('q') or SAVE_FLAG == True:
-            break      
+        app = None
+        SAVE_FLAG = False
+        LBUTTONDOWN_FLAG = False
+        LBUTTONUP_FLAG = False
+        MOUSEMOVE_FLAG = False
+        xy1 = [0, 0]#左クリックしたときの座標
+        xy2 = [0, 0]#左クリックを離したときのの座標
 
-    _xy1, _xy2 = Adjust(xy1, xy2) #枠がウィンドウを越えた場合に調整する
-    dst = Trimming(img, _xy1, _xy2)
-    
-    
-    result_file_path = file_path[0:file_path.rfind('.')] + '_result.jpg'
-    cv2.imwrite(result_file_path, dst)  
-    
-    cv2.destroyAllWindows()
+        
+        app = wx.App()
+        dialog = wx.FileDialog(None, u'トリミングしたいファイルを選択してください')
+        dialog.ShowModal()
+        file_path = dialog.GetPath()
+        img = cv2.imread(file_path)
+        
+        
+        if img is None:
+             dialog = wx.MessageBox(u'画像が読み込めません', u'ユーザーメッセージ')
+             break
+        
+        app = None     
+        CompositeImg = img.copy() 
+        cv2.namedWindow('title')
+        while(True):
+            cv2.setMouseCallback('title', mouse_event)
+            cv2.imshow('title', CompositeImg)
+            if cv2.waitKey(1) == ord('q') or SAVE_FLAG == True:
+                break
+            
+        if SAVE_FLAG == True:
+            _xy1, _xy2 = Adjust(xy1, xy2) #枠がウィンドウを越えた場合に調整する
+            dst = Trimming(img, _xy1, _xy2)              
+            result_file_path = file_path[0:file_path.rfind('.')] + '_result.jpg'
+            cv2.imwrite(result_file_path, dst)  
+        
+        cv2.destroyAllWindows() 
+        app = wx.App()
+        dlg = wx.MessageDialog(None, '別のファイルもトリミングしますか.','Message Dialog',
+                               wx.YES_NO | wx.ICON_QUESTION)
+        result = dlg.ShowModal()
+        if result == wx.ID_NO:
+            break
+        
     app = None
-
+        
+        
+        
+        
